f441137ebd8762d31cd364313a5bcae771b78462
pahnupong660610783 <pahnupong_kaeopramun@cmu.ac.th>
2023-12-15 14:07:25 +0700

    lab8

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -3,23 +3,41 @@
 #include<string>
 using namespace std;
 
-//[Missing Code 1] Write definition of the function findGrade() here.
+char findGrade(float x){
+        if (x>90){
+            return 'A';
+        }else if(x<=90 && x>75){
+            return 'B';
+        }
+        else if(x<=75 && x>60){
+            return 'C';
+         }
+        else if(x<=60 && x>45){
+            return 'D';
+        }
+        else {
+            return 'F';
+                } 
+        
+}
+
 
 int main(){
-	//Input the number of students
+	
 	int N,i = 0;
 	cout << "Enter the number of students: ";
 	cin >> N;
 	string name[N];
 	float score[N];	
 	
-	//Store names and scores of students into an array 
+  
 	while(i < N){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
-		//[Missing Code 2] Get name of the i-th students that may include whitespace.
+		getline(cin,name[i]);
 		cout << "Score of student " << i+1 << ": ";
-		//[Missing Code 3] Get score of the i-th students.
+		cin >>score[i];
+		
 		i++;
 	}
 	
--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,3 +1,6 @@
+#include<iostream>
+using namespace std;
+
 /*
 "Enter your age: "
 "Enter your height: "
@@ -11,3 +14,37 @@
 "Brook"
 "Jinbe"
 */
+int age,height,bounty;
+int main(){
+    cout <<"Enter your age: ";
+    cin >> age;
+    if (age<=25){
+      cout << "Enter your height: ";
+      cin >> height;
+        if (height<100){
+            cout << "Your character = "<<"Chopper";
+        }else if(height<180){
+             cout << "Your character = "<<"Usopp";       
+        }else{
+            cout <<"Enter your bounty: ";
+            cin >> bounty;
+            if(bounty>1100000000){
+                   cout <<"Your character = "<<"Zoro";
+            }else{
+                
+                cout <<"Your character = "<<"Sanji";
+            }
+        }
+    }else if (age<=60){
+        cout <<"Enter your bounty: ";
+        cin >> bounty;
+        if (bounty>500000000){
+        cout <<"Your character = "<<"Jinbe";       
+        }else{
+        cout <<"Your character = "<<"Franky";    
+        }
+        
+    }else{
+    cout <<"Your character = "<<"Brook";      
+    }
+}
